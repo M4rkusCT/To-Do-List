feac9a6b3d0586d6e6f5fb5215fe76f229e71e85
Marc Castro Trullá <marccastro02@gmail.com>
2022-11-27 19:42:22 +0100

    Design improvement

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 "use strict";
-const add = document.getElementById("submit");
+const add = document.getElementById("add");
+const clean = document.getElementById("clean");
 const list = document.getElementById("list");
-// const numDay = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-// const numMonth = [1,2,3,4,5,6,7,8,9,10,11,12];
 let arrTasks = JSON.parse(localStorage.getItem("List-tasks")) || [];
 
-const show = (arrTasks) => {
+const showTasks = (arrTasks) => {
     document.getElementById("list").innerHTML = "";
+    // const numDay = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+    // const numMonth = [1,2,3,4,5,6,7,8,9,10,11,12];
     // const date = new Date();
     for(let i = 0;i < arrTasks.length;i++) {
         document.getElementById("list").innerHTML += `<li class="task">${arrTasks[i]} <span class="delete" id=${i}>X</span></li>`;
@@ -14,7 +15,7 @@ const show = (arrTasks) => {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    show(arrTasks);
+    showTasks(arrTasks);
 });
 
 const saveTasksToStorage = (arrTasks) => {
@@ -24,15 +25,26 @@ const saveTasksToStorage = (arrTasks) => {
 add.addEventListener("click",(e) => {
     e.preventDefault();
     const task = document.getElementById("text");
-    arrTasks.push(task.value);
-    show(arrTasks);
-    saveTasksToStorage(arrTasks);
+    console.log(task)
+    if(task.value) {
+        arrTasks.push(task.value);
+        showTasks(arrTasks);
+        saveTasksToStorage(arrTasks);
+    }
+    else alert("You have to write a task.");
 });
 
 list.addEventListener("click", (e) => {
     e.preventDefault();
-    let pos = e.target.id;
-    arrTasks.splice(pos,1);
-    show(arrTasks);
-    saveTasksToStorage(arrTasks);
+    if(e.target.classList.contains("delete")) {
+        let pos = e.target.id;
+        arrTasks.splice(pos,1);
+        showTasks(arrTasks);
+        saveTasksToStorage(arrTasks);
+    }
+});
+
+clean.addEventListener("click", (e) => {
+    e.preventDefault();
+    
 });

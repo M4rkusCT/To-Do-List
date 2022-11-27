0125cd2afc6d66bce5f2371fa6394a8e80d40b2e
Marc Castro Trullá <marccastro02@gmail.com>
2022-11-27 20:30:18 +0100

    Clear LocalStorage list

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ let arrTasks = JSON.parse(localStorage.getItem("List-tasks")) || [];
 
 const showTasks = (arrTasks) => {
     document.getElementById("list").innerHTML = "";
-    // const numDay = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-    // const numMonth = [1,2,3,4,5,6,7,8,9,10,11,12];
-    // const date = new Date();
-    for(let i = 0;i < arrTasks.length;i++) {
-        document.getElementById("list").innerHTML += `<li class="task">${arrTasks[i]} <span class="delete" id=${i}>X</span></li>`;
+    if(arrTasks.length !== 0) {
+        for(let i = 0;i < arrTasks.length;i++) {
+            document.getElementById("list").innerHTML += `<li class="task">${arrTasks[i]}<span class="delete" id=${i}>X</span></li>`;
+        }
     }
 }
 
@@ -25,7 +24,6 @@ const saveTasksToStorage = (arrTasks) => {
 add.addEventListener("click",(e) => {
     e.preventDefault();
     const task = document.getElementById("text");
-    console.log(task)
     if(task.value) {
         arrTasks.push(task.value);
         showTasks(arrTasks);
@@ -46,5 +44,7 @@ list.addEventListener("click", (e) => {
 
 clean.addEventListener("click", (e) => {
     e.preventDefault();
-    
+    localStorage.clear();
+    arrTasks = [];
+    showTasks(arrTasks);
 });
